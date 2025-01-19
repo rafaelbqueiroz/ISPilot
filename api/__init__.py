db24001d3fab199d6cf2f2d49d15edc354f6d1b9
Rafa Queiroz <rafa@theailabs.co>
2025-01-19 14:56:59 -0300

    feat: Initial commit of project

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/__init__', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def handler():
     return jsonify({"status": "success", "message": "Endpoint funcionando"})
 
 if __name__ == "__main__":
-     from werkzeug.serving import run_simple
-     run_simple("0.0.0.0", 5000, app)
+  from werkzeug.serving import run_simple
+  run_simple("0.0.0.0", 5000, app)
